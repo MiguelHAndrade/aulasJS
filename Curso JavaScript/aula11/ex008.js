8595c2cc336a91be266f5c7b6c1fb722007728cd
Miguel Andrade <101271954+MiguelHAndrade@users.noreply.github.com>
2023-11-29 18:15:56 +0000

    aula11
    Novos exercícios dentro da aula 11.

---
+++ Curso JavaScript/aula11/ex008.js
@@ -0,0 +1,12 @@
+var vel = 12
+
+console.log(`A velocidade do carro é ${vel}Km/h.`)
+
+// Condição Simples
+if(vel >= 50) {
+    console.log('Você ultrepassou a velocidade permitida. Multado!')
+}
+
+// console.log('Você ultrepassou a velocidade permitida. Multado!')
+
+console.log('Conduza sempre com o sinto de segurança.')
---
+++ Curso JavaScript/aula11/ex009.js
@@ -0,0 +1,10 @@
+var pais = 'USA'
+// var pais = 'Portugal'
+
+console.log(`Se viver em ${pais}`)
+
+if(pais == 'Portugal'){
+    console.log('Você é Português')
+} else {
+    console.log('Você é Estrangeiro')
+}

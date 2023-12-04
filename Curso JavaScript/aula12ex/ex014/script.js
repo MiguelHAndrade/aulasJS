1cdef8e64fabd40737a51422710cc403027df49e
Miguel Andrade <101271954+MiguelHAndrade@users.noreply.github.com>
2023-12-04 17:39:59 +0000

    ex14
    ex14

---
+++ Curso JavaScript/aula12ex/ex014/script.js
@@ -0,0 +1,25 @@
+function carregar() {
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    var min = data.getMinutes()
+    var seg = data.getSeconds()
+    var msg2 = document.getElementById('msg2')
+    // var hora = 9
+
+    msg.innerHTML = `Agora são ${hora} horas, ${min} minutos e ${seg} seg.`
+
+    if (hora >= 8 && hora < 12) {
+        img.src = 'img/manha.png'
+        document.body.style.background = '#FFC166'
+    } else if (hora >= 12 && hora < 20) {
+        document.body.style.background = '#EF6326'
+    } else {
+        document.body.style.background = '#2C4C51'
+    }
+}
+
+function refresh() {
+    document.location.reload()
+}

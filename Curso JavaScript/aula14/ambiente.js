68243d57befeadded6c0b477830fd25bada15889
Miguel Andrade <101271954+MiguelHAndrade@users.noreply.github.com>
2023-12-06 11:21:47 +0000

    aula14
    aula14: "for"

---
+++ Curso JavaScript/aula14/ambiente.js
@@ -0,0 +1,12 @@
+// var c = 1
+
+// while (c <= 5) {
+//     console.log(c)
+//     c++
+// }
+
+console.log('Vai começar a contar...')
+for (var c2 = 1; c2 <= 3; c2++) {
+    console.log(`este nr é o ${c2}`)
+} 
+console.log('Acabou!')

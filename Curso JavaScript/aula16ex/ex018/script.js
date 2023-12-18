2d34b2476ef0a7f612b9d7235978964246cae728
Miguel Andrade <101271954+MiguelHAndrade@users.noreply.github.com>
2023-12-18 13:09:26 +0000

    ex018
    ex018: Completo!

--- Curso JavaScript/aula16ex/ex018/script.js
+++ Curso JavaScript/aula16ex/ex018/script.js
@@ -1,4 +1,5 @@
 let list = []
+let txt = document.getElementById('txtP')
 
 function add() {
     let number = document.getElementById('inpnr')
@@ -8,25 +9,35 @@ function add() {
         window.alert('Valor inválido, porfavor adicione um valor!')
     } else {
         let nr = Number(number.value)
-        let n = nr**2
-        if (nr > 0 && nr <= 100) {
-            list.push(nr)
-            let item = document.createElement('option')
-            item.text = `Valor ${nr} adicionado.`
-            item.value = `tab${nr}`
-            res.appendChild(item)
+        if (!inList(nr, list)) {
+            if (nr > 0 && nr <= 100) {
+                list.push(nr)
+                let item = document.createElement('option')
+                item.text = `Valor ${nr} adicionado.`
+                item.value = `tab${nr}`
+                res.appendChild(item)
+                number.value = ''
+                number.focus()
+                txt.innerHTML = ''
+            } else {
+                window.alert('Valor inválido, Porfavor insira um valor de 1 a 100!')
+            }      
         } else {
-            window.alert('Valor inválido, Porfavor insira um valor de 1 a 100!')
-        }
+            window.alert('Número inserido já foi adicionado, por favor adicione outro número diferente.')
+        } 
     }
-    // console.log(number.length)
 }
 
 console.log(list)
 
+function inList(n, l) {
+    return l.indexOf(n) !== -1
+}
+
 function show() {
     let r = document.getElementById('show')
-    let txt = document.getElementById('txtP')
+
+
 
     if(!txt) {
         txt = document.createElement('p')
@@ -43,12 +54,31 @@ function show() {
 
     let maiorNr = Math.max(...list)
 
+    let menorNr = Math.min(...list)
+
     media = soma/list.length
 
-    txt.innerHTML = `Ao todo temos ${list.length} números <br>
-    O maior valor informado foi ${maiorNr} <br>
-    A soma de todos os valores dá ${soma} <br>
-    A média de todos os valores dá ${media}`
+    // txt.innerHTML = `Ao todo temos <strong>${list.length}</strong> números. <br>
+    // <br>
+    // O maior valor informado foi <strong>${maiorNr}</strong>. <br>
+    // <br>
+    // O menor valor informado foi <strong>${menorNr}</strong>. <br>
+    // <br>
+    // A soma de todos os valores dá <strong>${soma}</strong>. <br>
+    // <br>
+    // A média de todos os valores dá <strong>${media}</strong>.` 
+    
+    txt.innerHTML = ''
+    txt.innerHTML += `Ao todo temos <strong>${list.length}</strong> números. <br><br>`
+    txt.innerHTML += `O maior valor informado foi <strong>${maiorNr}</strong>. <br><br>`
+    txt.innerHTML += `O menor valor informado foi <strong>${menorNr}</strong>. <br><br>`
+    txt.innerHTML += `A soma de todos os valores dá <strong>${soma}</strong>. <br><br>`
+    txt.innerHTML += `A média de todos os valores dá <strong>${media}</strong>. <br>`
+    
 
-    console.log(txt)
+    if (soma == 0) {
+        window.alert('ERRO!')
+        txt.innerHTML = ''
+    }
+   
 }
